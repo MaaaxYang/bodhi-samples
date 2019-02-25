9e3da0d322ead341f88e992ae8653b4af83d3847
杨浩然 <haoran_yang@bestsign.cn>
2019-02-25 23:05:42 +0800

    filePath jar

--- bodhi-client/src/main/java/com/bodhi/client/SimpleClient.java
+++ bodhi-client/src/main/java/com/bodhi/client/SimpleClient.java
@@ -11,9 +11,16 @@ import com.bodhi.core.utils.ClassUtil;
  **/
 public class SimpleClient {
 
-    public static void main(String[] args) throws Exception{
+    public static void main2(String[] args) throws Exception{
         ClassUtil.loadClassByApi("com.bodhi.server.Driver");
         ICar car = ClassUtil.loadClassByApi("com.bodhi.server.Benz");
         car.run();
+    }
+
+    public static void main(String[] args) throws Exception{
+        Object obj = ClassUtil.loadClassByFilePath(
+                "/Users/yanghaoran/coding/bodhi/lib/bodhi-server-1.0-SNAPSHOT.jar",
+                "com.bodhi.server.Driver");
+        Class cls = obj.getClass();
     }
 }
